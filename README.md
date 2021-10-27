786afab4531faddbbf15e7767ecc1ab2dfb4e355
Josey9087 <88466226+Josey9087@users.noreply.github.com>
2021-10-27 16:30:45 -0600

    Adding license description

--- README.md
+++ README.md
@@ -1,40 +1,76 @@
 
+![License](https://img.shields.io/badge/License-Unlicense-blue.svg)
 
-# Title: 
-jose
-
-
-Table of Contents:
-[Title](#title)
-[Description](#description)
-[Usage](#usage)
-[Installation](#installation)
-[Contribution Guidelines](#contribution-guidelines)
-[Testing Instuctions](#testing-instuctions)
-[Questions](#questions)
+# README Generator
 
 
+## Table of Contents:
 
-# Description:
+1.[Title](#title)
+
+2.[Description](#description)
+
+3.[Usage](#usage)
+
+4.[Installation](#installation)
+
+5.[Contribution Guidelines](#contribution-guidelines)
+
+6.[Testing Instuctions](#testing-instuctions)
+
+7.[Questions](#questions)
+
+8.[License](#license)
+
+
+
+## Description:
+Project for the University of Denver in order to create code that generates a professional README file
+
+
+## Usage: 
+It should be used in the temrinal to using node to generate a readme file
     
 
-
-# Usage: 
-
+## Installation: 
+use git clone to dowload the repository onto your computer, use the terminal to go into the directory of the repository, input npm init -y to get the npm package.json. follwing this use npm install inquirer in your temrinal to download the inquirer package. once done use node index.js to run the code and follow the prompts.
     
 
-# Installation: 
-
+## Contribution Guidelines:
+To make any contributions email me so we can discuss them
     
 
-# Contribution Guidelines:
-
+## Testing Instuctions:
+none
     
 
-# Testing Instuctions:
+## Questions:
+Github Username: Josey9087 Link to Github: https://github.com/Josey9087
+Email: yanezjose9087@gmail.com How to Reach Me: Reach out to me regarding this code with the subject README Generator
 
-    
 
-# Questions:
- 
 
+
+# License:
+This is free and unencumbered software released into the public domain.
+
+Anyone is free to copy, modify, publish, use, compile, sell, or
+distribute this software, either in source code form or as a compiled
+binary, for any purpose, commercial or non-commercial, and by any
+means.
+
+In jurisdictions that recognize copyright laws, the author or authors
+of this software dedicate any and all copyright interest in the
+software to the public domain. We make this dedication for the benefit
+of the public at large and to the detriment of our heirs and
+successors. We intend this dedication to be an overt act of
+relinquishment in perpetuity of all present and future rights to this
+software under copyright law.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
--- index.js
+++ index.js
@@ -1,108 +1,139 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
+const license1 = require('./generateLicense.js')
 // TODO: Create an array of questions for user input
-const generateReadMe = ({ title, description, usage, installation, contributions, test, github, email }) =>
-`
+const generateReadMe = ({ title, license, description, usage, installation, contributions, test, github, email, contact }) =>
+  `
+![License](https://img.shields.io/badge/License-${license}-blue.svg)
 
-# Title: 
-${title}
-
-
-Table of Contents:
-[Title](#title)
-[Description](#description)
-[Usage](#usage)
-[Installation](#installation)
-[Contribution Guidelines](#contribution-guidelines)
-[Testing Instuctions](#testing-instuctions)
-[Questions](#questions)
+# ${title}
 
 
+## Table of Contents:
 
-# Description:
-    ${description}
+1.[Description](#description)
+
+2.[Usage](#usage)
+
+3.[Installation](#installation)
+
+4.[Contribution Guidelines](#contribution-guidelines)
+
+5.[Testing Instuctions](#testing-instuctions)
+
+6.[Questions](#questions)
+
+7.[License](#license)
 
 
-# Usage: 
+
+## Description:
+${description}
+
+
+## Usage: 
 ${usage}
     
 
-# Installation: 
+## Installation: 
 ${installation}
     
 
-# Contribution Guidelines:
+## Contribution Guidelines:
 ${contributions}
     
 
-# Testing Instuctions:
+## Testing Instuctions:
 ${test}
     
 
-# Questions:
-Github Username: ${github} 
-Email: ${email}
+## Questions:
+Github Username: ${github} Link to Github: https://github.com/${github}
+Email: ${email} How to Reach Me: ${contact}
+
+
+
 `
-const questions = inquirer 
-.prompt ([
+const questions = inquirer
+  .prompt([
     {
-        type: 'input',
-        name: 'title',
-        message: 'What is the title of your project?',
-      },
-      {
-        type: 'list',
-        name: 'license',
-        message: 'Choose your license.',
-        choices: ["MIT", "GNU GPLv3", "None"]
-      },
-      {
-        type: 'input',
-        name: 'description',
-        message: 'Write a brief description of your project.',
-      },
-      {
-        type: 'input',
-        name: 'usage',
-        message: 'Write about how to code should be used.',
-      },
-      {
-        type: 'input',
-        name: 'installation',
-        message: 'Write about how users can install and use your code.',
-      },
-      {
-        type: 'input',
-        name: 'contributions',
-        message: 'Write about any contribution guidlines.',
-      },
-      {
-        type: 'input',
-        name: 'test',
-        message: 'Write testing instuctions',
-      },
-      {
-        type: 'input',
-        name: 'github',
-        message: 'What is your github username?',
-      },
-      {
-        type: 'input',
-        name: 'email',
-        message: 'What is your email?',
-      }
-])
+      type: 'input',
+      name: 'title',
+      message: 'What is the title of your project?',
+    },
+    {
+      type: 'list',
+      name: 'license',
+      message: 'Choose your license.',
+      choices: ["MIT", "Boost Software License 1.0", "Unlicense"]
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Write a brief description of your project.',
+    },
+    {
+      type: 'input',
+      name: 'usage',
+      message: 'Write about how to code should be used.',
+    },
+    {
+      type: 'input',
+      name: 'installation',
+      message: 'Write about how users can install and use your code.',
+    },
+    {
+      type: 'input',
+      name: 'contributions',
+      message: 'Write about any contribution guidlines.',
+    },
+    {
+      type: 'input',
+      name: 'test',
+      message: 'Write testing instuctions',
+    },
+    {
+      type: 'input',
+      name: 'github',
+      message: 'What is your github username?',
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: 'What is your email?',
+    },
+    {
+      type:'input',
+      name:'contact',
+      message:'How should other reach out to you by email?',
+    }
+  ])
 
-// TODO: Create a function to write README file
-.then((answers) => {
-    const READMECONTENT = generateReadMe(answers);
-    console.log(answers.license)
+  // TODO: Create a function to write README file
+  .then((answers) => {
+    doit=""
+    switch (answers.license) {
+      case 'MIT':
+        copyright = license1.MIT
+        break;
+      case 'Boost Software License 1.0':
+        copyright = license1.Boost
+        break;
+      case 'Unlicense':
+        copyright = license1.Unlicense
+        break;
 
+    }
+    const READMECONTENT = generateReadMe(answers,);
     fs.writeFile('README.md', READMECONTENT, (err) =>
       err ? console.log(err) : console.log('Successfully created README.md!')
     );
-    return answers.license
-  });
+    fs.appendFile('README.md', copyright, (err) =>
+    err ? console.log(err) : console.log('Successfully wrote licenses to README.md!') 
+  );
+  })
 
-  
+
+
+
