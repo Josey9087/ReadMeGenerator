4f7bbc2268abfad8a362262787e7636f62625709
Josey9087 <88466226+Josey9087@users.noreply.github.com>
2021-10-27 17:52:59 -0600

    Commenting generateLicense.js

--- generateLicense.js
+++ generateLicense.js
@@ -1,3 +1,4 @@
+// In here are the const variable that are used as the copyright licenses for the chosen license.
 const MIT=
 `## License:
 MIT License
@@ -73,6 +74,7 @@ OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
 ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
 OTHER DEALINGS IN THE SOFTWARE.`;
 
+// module.exports is used to exports the three variables above when another file calls to this one.
 module.exports={
     MIT,
     Boost,
